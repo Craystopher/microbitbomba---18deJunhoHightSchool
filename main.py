a832e9771bde08bd3a01d538b32db443a2bef0ac
Crys.Drm <116415720+Craystopher@users.noreply.github.com>
2025-02-24 23:54:54 -0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,123 @@
+for index in range(4):
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+for index2 in range(2):
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+basic.pause(500)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+basic.pause(500)
+for index3 in range(2):
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+basic.pause(500)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+    music.PlaybackMode.UNTIL_DONE)
+for index4 in range(2):
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    music.play(music.tone_playable(262, music.beat(BeatFraction.WHOLE)),
+        music.PlaybackMode.UNTIL_DONE)
+    basic.pause(500)
